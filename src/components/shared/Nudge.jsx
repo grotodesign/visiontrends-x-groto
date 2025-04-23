4019516b424897143bc93e96e276a9e5b3e3ea5c
abhinavmaharana <abhinavmaharana.004@gmail.com>
2025-04-23 23:52:00 +0530

    🐛 fixes in pointer

--- src/components/shared/Nudge.jsx
+++ src/components/shared/Nudge.jsx
@@ -10,12 +10,26 @@ import { Dialog,
     DialogHeader,
     DialogFooter,
     DialogTitle,
-    DialogDescription, } from "../ui/dialog"
+    DialogDescription
+} from "../ui/dialog"
+
+import pointerSvg from "../../assets/circles.svg"
 
 const Nudge = ({dialogOpen, setDialogOpen, positionCss='', title, info, buttonText, buttonText2, img, steps, currentStep, totalSteps, primaryAction, secondaryAction}) => {
   return (
-    <Dialog open={dialogOpen} onOpenChange={setDialogOpen} className="w-[200px] p-[0px]">
-        <DialogContent className={`${positionCss} p-[0px] max-w-[346px]`}>
+    <Dialog open={dialogOpen} onOpenChange={setDialogOpen} className="w-[200px] p-[0px] relative">
+        <DialogContent
+          className={`p-[0px] max-w-[346px] ${
+            title !== "Welcome to Vision Trends!" ? 'dialog-pointer' : ''
+          } ${positionCss}`}
+        >
+          {title !== "Welcome to Vision Trends!" && (
+            <img
+              src={pointerSvg}
+              alt="pointer"
+              className="absolute -top-[34px] left-1/2 transform -translate-x-1/2 w-[55px] h-auto -z-[100]"
+            />
+          )}
           <DialogHeader>
             <DialogDescription>
                 <div className="h-[200px] w-full">
